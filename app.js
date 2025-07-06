19b80b8491df4c6cc71a371126b919840c0866e7
bharathloganathan <bharathloganathan2714@gmail.com>
2025-07-06 15:38:03 +0530

    updated for hosting

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ sequelize
   });
 
 // Start the server
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.DB_PORT;
 
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, 'mysql.railway.internal', () => {
   console.log(`Server is running on ${PORT}`);
 });
 
